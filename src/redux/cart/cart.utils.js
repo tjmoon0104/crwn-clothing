17bad419f63e85451942ae1f537824b989d19bb9
tjmoon0104 <tjmoon0104@outlook.com>
2020-11-29 22:33:04 +0100

    Add Cart Item

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,12 +4,13 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   )
 
   if (existingCartItem) {
-    return cartItems.map(
-      (cartItem) =>
-        cartItem.id === cartItemToAdd.id && {
-          ...cartItem,
-          quantity: cartItem.quantity + 1,
-        }
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? {
+            ...cartItem,
+            quantity: cartItem.quantity + 1,
+          }
+        : cartItem
     )
   }
 
