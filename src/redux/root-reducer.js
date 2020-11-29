219b01631b45c7b1e8b5c0eca57529827a65a287
tjmoon0104 <tjmoon0104@outlook.com>
2020-11-29 09:29:49 +0100

    Cart Icon, Cart Dropdown, Cart Reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import userReducers from './user/user.reducers'
+import cartReducer from './cart/cart.reducers'
 
 export default combineReducers({
   user: userReducers,
+  cart: cartReducer,
 })
